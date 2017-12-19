5a7fcec32debbfc91038cc0b6fe7dd63dd44d061
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-19 15:27:25 +0700

    try to made modal

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ import {
 	Platform,
 	StyleSheet,
 	Text,
-	View
+	View,
+	Button,
+	Modal,
+	TouchableWithoutFeedback
 } from 'react-native';
 import NoteEditor from './app/components/NoteEditor/NoteEditor.component';
 
@@ -21,10 +24,40 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+	state = {
+		modalVisible: false,
+	};
+
+	openModal = () => {
+		this.setState({modalVisible:true});
+	}
+	
+	closeModal = () => {
+		this.setState({modalVisible:false});
+	}
+
+	stopPropagation = (event) => {
+		event.stopPropagation();
+	}
+	
 	render() {
 		return (
 			<View style={styles.container}>
 				<NoteEditor/>
+				<Button title="open modal" onPress={this.openModal}/>
+				
+				<Modal transparent={true} visible={this.state.modalVisible} onRequestClose={this.closeModal}>
+					<TouchableWithoutFeedback onPress={this.closeModal}>
+						<View style={styles.modalMainContainer}>
+							<TouchableWithoutFeedback onPress={this.stopPropagation}>
+								<View style={styles.modalContentContainer}>
+									<Text>This is content inside of modal component</Text>
+									<Button title="Close modal" onPress={this.closeModal}/>
+								</View>
+							</TouchableWithoutFeedback>
+						</View>
+					</TouchableWithoutFeedback>
+				</Modal>
 			</View>
 		);
 	}
@@ -48,4 +81,19 @@ const styles = StyleSheet.create({
 	// 	color: '#333333',
 	// 	marginBottom: 5,
 	// },
+	modalMainContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'rgba(37, 8, 10, 0.28)',
+		padding: 10,
+	},
+	modalContentContainer: {
+		width: '100%',
+		height: 200,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'white',
+		borderRadius: 10,
+	},
 });
